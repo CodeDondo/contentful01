c5723bf2ffb299c7e90abc39c91e4d2974da921e
JustDondo <162633910+CodeDondo@users.noreply.github.com>
2025-02-04 10:57:43 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Header } from './components/header/Header'
+import { Main } from './components/main/Main'
+import { Footer } from './components/footer/Footer'
+import './App.scss'
+
+function App() {
+
+  return (
+    <>
+      <Header className="reset"> 
+        <h1>Velkommen</h1>
+      </Header>
+      <Main> 
+        <h2>Dette er main</h2>
+      </Main>
+      <Footer>
+        <h3>Footer</h3>
+        <a href="#">Test</a>
+      </Footer>
+    </>
+  )
+}
+
+export default App
