8845e6ef75becb2632f0234431f4ffe0b7a75ef8
JustDondo <162633910+CodeDondo@users.noreply.github.com>
2025-02-06 09:18:43 +0100

    Tilføjet header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,25 @@
 import { HeaderStyled } from "./HeaderStyled";
+import { client } from "../../contentful.js";
+import { useEffect, useState } from "react";
 
-export const Header = () => {
-    return (
-        <HeaderStyled>
-        <h1>Header</h1>
-        </HeaderStyled>
-    );
+client.getEntry("4Qs8UAPOUAccLNUQ3rFK4N")
+    .then((entry) => console.log(entry))
+    .catch(console.error);
+
+export const Header = () => {  // Named export
+    const [data, setData] = useState()
+
+
+    useEffect(() => {
+        client.getEntry("4Qs8UAPOUAccLNUQ3rFK4N")
+            .then((entry) => setData(entry))
+            .catch(console.error);
+    }, [])
+    console.log(data)
+
+        return (
+            <HeaderStyled>
+                <h1>{data?.fields.title}</h1>
+            </HeaderStyled>
+        );
     };
